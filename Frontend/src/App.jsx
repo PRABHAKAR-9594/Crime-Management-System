ae7d5050b5732f565c7e557eb1a8eccc6605c702
Dinesh-171 <dineshpadhi16@gmail.com>
2025-01-02 20:43:31 +0530

    initialize frontend with Vite and React setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 align="center">Crime Management System</h2>
+    </>
+  )
+}
+
+export default App
