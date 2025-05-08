2733653249bf5a76db57b5d05e4f5e7911ad8bf4
Prabhakar maurya <mauryaprabhakar677@gmail.com>
2025-05-08 17:31:06 +0530

    Accha

--- Backend/Routes/verifyMobile.js
+++ Backend/Routes/verifyMobile.js
@@ -1,5 +1,5 @@
-import { verifyMobile } from "../Controllers/verifyMobile.controller.js"
-import { newPassword } from "../Controllers/verifyMobile.controller.js"
+import { verifyMobile } from "../controllers/verifyMobile.controller.js"
+import { newPassword } from "../controllers/verifyMobile.controller.js"
 import { verifyMobile_middleware } from "../Middlewares/verifyMobile.middleware.js"
 export const verify_Mobile= (app)=>{
     app.post('/verifyMobile',[verifyMobile_middleware],verifyMobile)
